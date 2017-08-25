297267d07bb6bdcf95a80840a913eeedaab9897a
Pavel Zika <pzika@langmaster.cz>
2017-08-25 12:24:43 +0200

    v

--- polyfill/index.tsx
+++ polyfill/index.tsx
@@ -1,6 +1,4 @@
-﻿export { Text, View, Button, TouchableHighlight, TouchableOpacity, Platform, I18nManager } from 'react-native'
-export { TabNavigator, StackNavigator } from 'react-navigation'
+﻿//export { Text, View, Button, TouchableHighlight, TouchableOpacity, Platform, I18nManager } from 'react-native'
+//export { TabNavigator, StackNavigator } from 'react-navigation'
 
-//export { Text, View, Button, TouchableHighlight, TouchableOpacity, Platform, I18nManager } from '../app-web/react-native/index'
-
-
+export { Text, View, Button, TouchableHighlight, TouchableOpacity, Platform, I18nManager } from '../app-web/react-native/index'
