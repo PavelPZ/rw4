7d76bbe8f74a6e25284b973f66d517ee15bc0d2b
Pavel Zika <pzika@langmaster.cz>
2017-09-17 21:22:54 +0200

    v

--- app-web/snack/animated-transition.tsx
+++ app-web/snack/animated-transition.tsx
@@ -1,6 +1,5 @@
 ﻿//https://restlessbit.github.io/react-transition-demo/index.html
 //https://github.com/restlessbit/react-transition-demo
-
 //https://reactcommunity.org/react-transition-group/
 import React from 'react'
 import Transition from 'react-transition-group/Transition'
@@ -13,42 +12,41 @@ const enum TTransitionState {
   exiting = 'exiting',
 }
 
-const Card: React.SFC<any> = ({ children, onRemove }) => <div>
-  {children}
-  <button onClick={onRemove}>Remove</button>
-</div>
+interface IFadeProps {
+  duration: number
+  in?: boolean,
+  //childs: React.ReactNode
+}
 
-const FadeTransition: React.SFC<any> = ({ children, duration, in: inProp }) => <Transition in={inProp} timeout={{ enter: 0, exit: duration }}>
-  {(state: TTransitionState) => {
-    if (state === TTransitionState.exited) return null;
-    return React.cloneElement(children, { style: { ...defaultStyle(duration), ...transitionStyles[state] } })
-  }}
+const FadeTransition: React.SFC<IFadeProps> = ({ duration, in: inProp, children }) => <Transition in={inProp} timeout={{ enter: 0, exit: duration }}>
+  {(state: TTransitionState) => state === TTransitionState.exited ? null : <div style={getStyle(duration, state)}>{children}{` STATE: ${state}, count: ${count++}`}</div>}
 </Transition>
+let count = 0
 
-const defaultStyle = duration => ({
+const getStyle = (duration, state) => ({
   transition: `${duration}ms ease-in`,
   position: 'absolute',
   height: '30px',
-  top:'-30px',
-  transitionProperty: 'opacity, transform'
+  transitionProperty: 'opacity, transform',
+  ...transitionStyles[state]
 } as CSSProperties)
 
 const transitionStyles = {
   entering: {
     opacity: 0,
     transform: 'translateY(-30px)'
-  },
+  } as CSSProperties,
   entered: {
     opacity: 1,
     transform: 'translateY(0)'
-  },
+  } as CSSProperties,
   exiting: {
     opacity: 0,
     transform: 'translateY(30px)'
-  }
+  } as CSSProperties
 }
 
-const Board: React.SFC<any> = ({ children }) => <div style={{ position: 'relative', paddingTop:'30px' }}>{children}</div>
+const Board: React.SFC<any> = ({ children }) => <div style={{ height: '60px', position: 'relative', overflow: 'hidden', border: 'solid 1px black', marginTop: '10px', padding: '15px' }}>{children}</div>
 
 export default class ReactTransitionDemo extends React.Component {
 
@@ -56,7 +54,7 @@ export default class ReactTransitionDemo extends React.Component {
 
   render() {
     const { cards } = this.state;
-
+    const duration = 300
     return (
       <div>
         <h1>React Transition Demo</h1>
@@ -64,10 +62,8 @@ export default class ReactTransitionDemo extends React.Component {
         <button onClick={() => this.removeLastCard()}>Remove last</button>
         <button onClick={() => this.replaceCard()}>Replace</button>
         <TransitionGroup component={Board}>
-          {cards.map(card => <FadeTransition duration={1500} key={card.id} in={undefined}>
-            <div>
-              {card.content}{' '} <button onClick={() => this.removeCard(card.id)}>Remove</button>
-            </div>
+          {cards.map(card => <FadeTransition duration={1000} key={card.id}>
+            {card.content}{' '}<button onClick={() => this.removeCard(card.id)}>Remove</button>
           </FadeTransition>
           )}
         </TransitionGroup>
