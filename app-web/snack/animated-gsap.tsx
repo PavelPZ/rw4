c38ad63ce4926769208740ee03397abfb39b5688
Pavel Zika <pzika@langmaster.cz>
2017-09-18 14:47:13 +0200

    v

--- app-web/snack/animated-gsap.tsx
+++ app-web/snack/animated-gsap.tsx
@@ -10,63 +10,67 @@ const app1: React.SFC<any> = props => <div style={{ position: 'relative' }}>
 </div>
 
 const Root: React.SFC<any> = props => {
-  let router
+  let router: Router
+  const getNextContent = (router: Router) => <Content onRef={root => router.rootExist(root)} count={count} key={count++} />
+  let count = 1
   return <div style={{ display: 'flex', flex: 1, flexDirection: 'column', height: '100vh' }}>
     <div style={{ alignSelf: 'flex-start' }}>
-      <button onClick={() => router.click()}>CLICK</button>
+      <button onClick={() => router.setContent(getNextContent(router))}>CLICK</button>
     </div>
-    <Router ref={r => router = r} />
+    <Router ref={r => router = r} getInitContent={getNextContent} />
   </div>
 }
 
 type IProps = { title: string; margin: number }
 
-class Router extends React.PureComponent {
-  state = {
-    count: count++
-  }
+class Router extends React.PureComponent<{ getInitContent }> {
+  state = { content: this.props.getInitContent(this) }
   root: HTMLDivElement
   firstTween: gsap.TweenLite
   secondTween: gsap.TweenLite
-  onRef: (root: HTMLDivElement) => void
-  doOnRef(root: HTMLDivElement) {
+  onRootExist: () => void
+  rootExist(root: HTMLDivElement) {
     this.root = root
-    if (this.onRef) this.onRef(root)
+    if (this.onRootExist) this.onRootExist()
   }
+  cancel = { cancel: null }
   render() {
     return <div style={{ display: 'flex', flex: 1, flexDirection: 'column' }}>
-      <Content onRef={root => this.doOnRef(root)} key={this.state.count} count={this.state.count} />
+      {this.state.content}
     </div>
   }
-  click() {
-    this.onRef = null
+  async setContent(content: JSX.Element) {
+    this.onRootExist = null
     if (this.firstTween) {
       this.firstTween.progress(1, true); this.firstTween = null
-      this.setState({ count: count++ })
-      this.setState({ count: count++ })
-      return 
+      this.setState({ content: content })
+      return
     } else if (this.secondTween) {
       this.secondTween.progress(1, true); this.secondTween = null
-      this.setState({ count: count++ })
-      return 
+      this.setState({ content: content })
+      return
     }
     this.firstTween = TweenLite.to(this.root, tweenTime, {
-      opacity: 0.5, onComplete: async () => {
+      opacity: 0.5, onComplete: () => {
         this.firstTween = null
-        this.onRef = root => {
-          this.secondTween = TweenLite.from(root, tweenTime, {
-            opacity: 0.5, onComplete: () => {
-              this.secondTween = null
-            }
-          })
-        }
-        this.setState({ count: count++ })
+        this.onRootExist = () => this.secondTween = TweenLite.from(this.root, tweenTime, { opacity: 0.5, onComplete: () => this.secondTween = null })
+        this.setState({ content: content })
       }
     })
   }
 }
-let count = 1
 const tweenTime = 1
+
+const doTween = (el: HTMLElement, secs: number, pars: {}, cancel: { cancel }, fnc?) => {
+  const res = new Promise<void>(resolve => {
+    let tween = (fnc || TweenLite.to)(el, secs, { ...pars, completed: () => { delete cancel.cancel; resolve() } })
+    cancel.cancel = () => {
+      tween.progress(1, true)
+      delete cancel.cancel
+      return true
+    }
+  })
+}
 
 class Content extends React.PureComponent<{ count: number, onRef: (root: HTMLDivElement) => void }> {
   render() {
