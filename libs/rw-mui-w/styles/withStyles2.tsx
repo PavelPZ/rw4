89614331768acbb13ca1759946a0455b625a6f65
Pavel Zika <pzika@langmaster.cz>
2017-11-26 00:06:43 +0100

    v

--- libs/rw-mui-w/styles/withStyles2.tsx
+++ libs/rw-mui-w/styles/withStyles2.tsx
@@ -10,8 +10,8 @@ function beforeWithStyleMui<C, R extends Mui2.TypedRules>(Component: Mui2.origCo
   type TKey = keyof R
   const res: Mui2.SFC<C, R> = (props: Mui2.Props<{}, Mui2.TypedRules>) => {
     const { classes: inlineClasses, style, ...rest } = props
-    const classes = stylesToClassNames(inlineClasses)
-    const webProps = { style: expanRule(style), classes, ...rest } as (C & Mui2.origStyledComponentProps<TKey>)
+    const classes = stylesToClassNames(expanRulesWeb(inlineClasses) as Mui2.ExpandedRulesWeb<R>)
+    const webProps = { style: expanRuleWeb(style), classes, ...rest } as (C & Mui2.origStyledComponentProps<TKey>)
     return <Component {...webProps} />
   }
   return hoistNonReactStatics(res, Component)
@@ -23,7 +23,7 @@ const muiBeforeWithStyleMui = <C, R extends Mui2.TypedRules, TKey extends string
   const res: React.SFC<Mui2.muiProps<C, R, TKey>> = (props: Mui2.muiProps<{}, Mui2.TypedRules, string>) => {
     const { classes: { web: inlineStyles }, style, ...rest } = props
     const classes = stylesToClassNames(inlineStyles)
-    const webProps = { style: expanRule(style), classes, ...rest } as (C & Mui2.origStyledComponentProps<TKey>)
+    const webProps = { style: expanRuleWeb(style), classes, ...rest } as (C & Mui2.origStyledComponentProps<TKey>)
     return <Component {...webProps} />
   }
   return hoistNonReactStatics(res, Component)
@@ -31,15 +31,15 @@ const muiBeforeWithStyleMui = <C, R extends Mui2.TypedRules, TKey extends string
 
 export const muiWithStyleWeb: Mui2.muiWithStyleWeb = (style, options) => comp => muiBeforeWithStyleMui(origWithStyles(style, options)(comp as Mui2.origToWithStyleComponentType<any, string>))
 
-function expanRule<T extends Mui2.NativeStyle>(rule: Mui2.Rule<T>): Mui2.ExpandedRule<T> {
+function expanRuleWeb<T extends Mui2.NativeStyle>(rule: Mui2.Rule<T>): Mui2.ExpandedRule<T> {
   if (!rule) return null
   const { web, native, ...rest } = rule
   return { ...rest, ...web }
 }
 
-function expanRulesProc<R extends Mui2.TypedRules>(rules: Mui2.Rules<R>): Mui2.ExpandedRules<R> {
+function expanRulesWeb<R extends Mui2.TypedRules>(rules: Mui2.Rules<R>): Mui2.ExpandedRules<R> {
   if (!rules) return null
   const res: Mui2.ExpandedRules<R> = {}
-  for (const p in rules) res[p] = expanRule(rules[p])
+  for (const p in rules) res[p] = expanRuleWeb(rules[p])
   return res
 }
